adc2d203879550c3188c3dc706e27044e123dcc2
rbinder <robinbinder1@gmx.de>
2019-04-22 10:13:45 +0200

    Inital commit

---
+++ src/test/java/com/rbinder/hsas/tempservice/TempServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rbinder.hsas.tempservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TempServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
